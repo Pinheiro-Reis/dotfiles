1499781aec33c4166c836d263e394a3082b74a32
João Victor <joaovictorpinheiro2007@gmail.com>
2025-07-02 16:50:38 -0300

    nvim: Change M.ui to include tailwind

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -12,9 +12,17 @@ M.base46 = {
 
 M.nvdash = { load_on_startup = true }
 M.ui = {
-      tabufline = {
+    tabufline = {
          lazyload = false
-     }
+    },
+
+    cmp = {
+      lspkind_text = true,
+      style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+      format_colors = {
+         tailwind = true,
+      },
+    },
 }
 M.colorify = { enabled = true }
 
