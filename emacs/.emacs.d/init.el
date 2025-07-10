623b6ae56e193e9e9d833c77c7803735babae711
João Victor <joaovictorpinheiro2007@gmail.com>
2025-07-10 19:17:11 -0300

    change windows size to fill all screen and decrease status bar

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,3 +2,15 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (set-frame-font "JetBrainsMono Nerd Font 14" nil t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
