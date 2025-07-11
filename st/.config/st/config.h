2837085e90aab02a3a4645cead7cd966f8dd730b
João Victor <joaovictorpinheiro2007@gmail.com>
2025-07-11 14:52:39 -0300

    stow: add global config. st: restore def colors

--- st/.config/st/config.h
+++ st/.config/st/config.h
@@ -91,7 +91,7 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 4;
 
 /* bg opacity */
 float alpha = 1;
@@ -110,7 +110,7 @@ static const char *colorname[] = {
 	"#81a1c1", /* blue    */
 	"#b48ead", /* magenta */
 	"#88c0d0", /* cyan    */
-	"#e5e9f0", /* white   */
+	"#ffffff", /* white   */
 
 	/* 8 bright colors */
 	"#000000", /* black   */
@@ -120,12 +120,12 @@ static const char *colorname[] = {
 	"#81a1c1", /* blue    */
 	"#b48ead", /* magenta */
 	"#8fbcbb", /* cyan    */
-	"#eceff4", /* white   */
+	"#ffffff", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#d8dee9", /* default foreground colour */
+	"#ffffff", /* default foreground colour */
 	"#000000", /* default background colour */
 };
 
@@ -134,10 +134,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 256;
-unsigned int defaultbg = 257;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
